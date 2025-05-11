77c1f85f6f411264406909fe69c6493bf840bfc6
buzhidaoqsm <1195212504@qq.com>
2025-05-11 16:35:18 +0800

    Implement handling the action of moving a player

--- src/main/java/com/island/controller/ActionBarController.java
+++ src/main/java/com/island/controller/ActionBarController.java
@@ -2,6 +2,10 @@ package com.island.controller;
 
 import com.island.model.Island;
 import com.island.model.Player;
+import com.island.model.Tile;
+import com.island.model.Position;
+
+import java.util.List;
 
 public class ActionBarController {
     private GameController gameController;
@@ -42,28 +46,57 @@ public class ActionBarController {
     }
 
     public boolean canPlaySpecialCard(Player player) {
+        return gameController.getPlayerController().canPlaySpecialCard(player);
     }
 
     public boolean canShoreUpTile(Player player) {
+        return gameController.getPlayerController().canShoreUpTile(player);
     }
 
     public boolean canGiveCard(Player player) {
+        return gameController.getPlayerController().canGiveCard(player);
     }
 
     public boolean canCaptureTreasure(Player player) {
+        return gameController.getPlayerController().canCaptureTreasure(player);
     }
 
     public boolean hasDrawnTreasureCards() {
+        return gameController.getPlayerController().hasDrawnTreasureCards();
     }
 
     public void sendDrawTreasureCardsMessage(int i, Player player) {
+        gameController.getRoomController().sendDrawTreasureCardsMessage(i, player);
     }
 
     public int getDrawnFloodCards() {
+        return gameController.getPlayerController().getDrawnFloodCards();
     }
 
+    /**
+     * Handles the action of moving a player.
+     * This method checks if the player is on a sunk tile and if the chosen tile is valid for movement.
+     * If valid, it sends a move message to the room controller.
+     */
     public void handleMoveAction() {
-
+        Tile chosenTile = gameController.getChosenTile();
+        Tile playerTile = getIsland().getTile(currentPlayer.getPosition());
+        if (playerTile.isSunk()) {
+            List<Tile> validTilesOnSunk = gameController.getValidTilesOnSunk(currentPlayer);
+            if (chosenTile != null && validTilesOnSunk.contains(chosenTile)) {
+                gameController.getRoomController().sendMoveMessage(currentPlayer, chosenTile.getPosition());
+            } else {
+                gameController.showErrorToast("Invalid Tile!");
+            }
+        }
+        if (getRemainingActions() > 0) {
+            List<Position> validPositions = currentPlayer.getMovePositions(getIsland().getTiles());
+            if (chosenTile != null && validPositions.contains(chosenTile.getPosition())) {
+                gameController.getRoomController().sendMoveMessage(currentPlayer, chosenTile.getPosition());
+            } else {
+                gameController.showErrorToast("Invalid Move!");
+            }
+        }
     }
 
     public void handleShoreUpAction() {
