e493054d4faea0071f0e0fb02ade8e91cbb56a80
Yuhan Zhong <yuhan.zhong@ucdconnect.ie>
2025-05-29 19:43:31 +0800

    Delete src/main/java/com/island/model/GameState.java

--- src/main/java/com/island/model/GameState.java
+++
@@ -1,9 +0,0 @@
-package com.island.model;
-
-public enum GameState {
-    INITIALIZING,   // Initialization
-    RUNNING,        // Game running
-    PAUSED,         // Game paused
-    GAME_OVER,      // Game over
-    TURN_START,     // Turn start
-}
